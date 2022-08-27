40ff9a3b7ed8e62757260dbe40efef7e074c3113
NOoBODDY <offmysoap@gmail.com>
2022-08-27 15:02:59 +0300

    Added calc class

---
+++ ArithmeticParser/ArithmeticBigIntCalc.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace ArithmeticParser;
+
+public class ArithmeticBigIntCalc
+{
+    private readonly string _input;
+
+    public ArithmeticBigIntCalc(string input) => _input = input;
+    public BigInteger Calc()
+    {
+        var Ling = new LinguisticAnalyzer(_input);
+        var tokens = Ling.ParseTokens();
+        var Syntax = new SyntaxParserBigInt(tokens);
+        return Syntax.Parse();
+    }
+}
---
+++ ArithmeticParserTest/ArithmeticBigIntCalcTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+using ArithmeticParser;
+
+namespace ArithmeticParserTest;
+
+public class ArithmeticBigIntCalcTest
+{
+    [Fact]
+    public void SimpleExpression()
+    {
+        string input = "345 + 432 - 123";
+        ArithmeticBigIntCalc parser = new ArithmeticBigIntCalc(input);
+
+        BigInteger result = parser.Calc();
+        
+        Assert.Equal(new BigInteger(654), result);
+    }
+    [Fact]
+    public void SimpleExpressionWithBrackets()
+    {
+        string input = "345 + (432 -123)";
+        ArithmeticBigIntCalc parser = new ArithmeticBigIntCalc(input);
+
+        BigInteger result = parser.Calc();
+        
+        Assert.Equal(new BigInteger(654), result);
+    }
+    
+    [Fact]
+    public void ArithmeticOrderTest()
+    {
+        string input = "345 * (432 -123)";
+        ArithmeticBigIntCalc parser = new ArithmeticBigIntCalc(input);
+
+        BigInteger result = parser.Calc();
+        
+        Assert.Equal(new BigInteger(106605), result);
+    }
+    
+    [Fact]
+    public void ArithmeticOrderMoreBracketsTest()
+    {
+        string input = "(43+12)*54 -(234 + 35 * (43 -23))";
+        ArithmeticBigIntCalc parser = new ArithmeticBigIntCalc(input);
+
+        BigInteger result = parser.Calc();
+        
+        Assert.Equal(new BigInteger(2036), result);
+    }
+    [Fact]
+    public void BigIntTestTest()
+    {
+        string input = "345735645345345 * (432234234999234234 -123234222344)";
+        ArithmeticBigIntCalc parser = new ArithmeticBigIntCalc(input);
+
+        BigInteger result = parser.Calc();
+        
+        Assert.Equal(BigInteger.Parse("149438739571348363549307081152050"), result);
+    }
+}
