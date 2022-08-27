5f4b5960081a746d71d14e0a7768cb1b307e90a3
NOoBODDY <offmysoap@gmail.com>
2022-08-27 15:02:41 +0300

    Added LinguisticAnalyzer

---
+++ ArithmeticParser/LinguisticAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ArithmeticParser;
+/// <summary>
+/// [0..9] - число
+/// - + / * - арифметические знаки
+/// ( ) - скобки
+/// </summary>
+public class LinguisticAnalyzer
+{
+    private readonly string _line;
+    private readonly Regex _regex;
+    public LinguisticAnalyzer(string line)
+    {
+        _line = line;
+        string pattern = @"(?<NUM>\d+)+?|(?<PLUS>[+]){1}|(?<MINUS>[-]){1}|(?<LEFTBR>[(]){1}|(?<RIGHTBR>[)]){1}|(?<MULT>[*]){1}|(?<DIV>[/]){1}";
+        _regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+    }
+    public string[] ParseTokens()
+    {
+        var matches = _regex.Matches(_line);
+        return matches.Select(x => x.Value).ToArray();
+    }
+    
+}
---
+++ ArithmeticParserTest/LinguisticAnalyzerTest.cs
@@ -0,0 +1,59 @@
+﻿using ArithmeticParser;
+
+namespace ArithmeticParserTest;
+
+public class LinguisticAnalyzerTest
+{
+    [Fact]
+    public void NumericTokensTest()
+    {
+        string line = "42345456 123435 63423 41234";
+        var analyzer = new LinguisticAnalyzer(line);
+
+        string[] tokens = analyzer.ParseTokens();
+        
+        Assert.Equal(new string[] {"42345456", "123435", "63423", "41234"}, tokens);
+    }
+    
+    [Fact]
+    public void NumericAndArithmeticSignsSplitedBySpaceTokensTest()
+    {
+        string line = "42345456 + 123435 - 63423 41234";
+        var analyzer = new LinguisticAnalyzer(line);
+
+        string[] tokens = analyzer.ParseTokens();
+        
+        Assert.Equal(new string[] {"42345456","+", "123435","-", "63423", "41234"}, tokens);
+    }
+    [Fact]
+    public void NumericAndArithmeticSignsWithoutSpaceTokensTest()
+    {
+        string line = "42345456+123435-63423 41234";
+        var analyzer = new LinguisticAnalyzer(line);
+
+        string[] tokens = analyzer.ParseTokens();
+        
+        Assert.Equal(new string[] {"42345456","+", "123435","-", "63423", "41234"}, tokens);
+    }
+    [Fact]
+    public void NumericAndArithmeticSignsAndBracketsWithoutSpaceTokensTest()
+    {
+        string line = "42345456+(123435-63423)- 41234";
+        var analyzer = new LinguisticAnalyzer(line);
+
+        string[] tokens = analyzer.ParseTokens();
+        
+        Assert.Equal(new string[] {"42345456","+","(", "123435","-", "63423",")","-", "41234"}, tokens);
+    }
+    
+    [Fact]
+    public void AllLexTest()
+    {
+        string line = "-2342*((423+34)/3 +42345456+(123435-63423))- 41234";
+        var analyzer = new LinguisticAnalyzer(line);
+
+        string[] tokens = analyzer.ParseTokens();
+        
+        Assert.Equal(new string[] {"-","2342","*","(","(","423","+","34",")","/","3","+","42345456","+","(", "123435","-", "63423",")",")","-", "41234"}, tokens);
+    }
+}
