9e589c4b4ca8750809b566a59259ee2b1db0876f
NOoBODDY <offmysoap@gmail.com>
2022-08-27 18:01:06 +0300

    Added arithmetic sign statistics

--- ArithmeticParser/LinguisticAnalyzer.cs
+++ ArithmeticParser/LinguisticAnalyzer.cs
@@ -10,6 +10,12 @@ public class LinguisticAnalyzer
 {
     private readonly string _line;
     private readonly Regex _regex;
+    
+    public int PlusCount { get; private set; }
+    public int MinusCount { get; private set; }
+    public int MultCount { get; private set; }
+    public int DivCount { get; private set; }
+    
     public LinguisticAnalyzer(string line)
     {
         _line = line;
@@ -18,8 +24,25 @@ public class LinguisticAnalyzer
     }
     public string[] ParseTokens()
     {
+        PlusCount = 0;
+        MinusCount = 0;
+        MultCount = 0;
+        DivCount = 0;
         var matches = _regex.Matches(_line);
+        foreach (Match match in matches)
+        {
+            var groups = match.Groups;
+            if (groups["PLUS"].Success)
+                PlusCount++;
+            if (groups["MINUS"].Success)
+                MinusCount++;
+            if (groups["MULT"].Success)
+                MultCount++;
+            if (groups["DIV"].Success)
+                DivCount++;
+        }
         return matches.Select(x => x.Value).ToArray();
+        
     }
     
 }
--- ArithmeticParserTest/LinguisticAnalyzerTest.cs
+++ ArithmeticParserTest/LinguisticAnalyzerTest.cs
@@ -56,4 +56,18 @@ public class LinguisticAnalyzerTest
         
         Assert.Equal(new string[] {"-","2342","*","(","(","423","+","34",")","/","3","+","42345456","+","(", "123435","-", "63423",")",")","-", "41234"}, tokens);
     }
+    
+    [Fact]
+    public void SignCountTest()
+    {
+        string line = "42345456+(123435-63423)- 41234";
+        var analyzer = new LinguisticAnalyzer(line);
+
+        string[] tokens = analyzer.ParseTokens();
+        
+        Assert.Equal(1, analyzer.PlusCount);
+        Assert.Equal(2, analyzer.MinusCount);
+        Assert.Equal(0, analyzer.MultCount);
+        Assert.Equal(0, analyzer.DivCount);
+    }
 }
