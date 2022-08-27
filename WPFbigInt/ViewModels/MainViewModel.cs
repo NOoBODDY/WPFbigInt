40f2accee44701861e4e2b713472ae5ca178a940
NOoBODDY <offmysoap@gmail.com>
2022-08-27 12:06:13 +0300

    Add MVVM beginning

---
+++ WPFbigInt/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFbigInt.ViewModels;
+
+public class MainViewModel: INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- WPFbigInt/Views/MainWindow.xaml.cs
+++ WPFbigInt/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFbigInt
+namespace WPFbigInt.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
